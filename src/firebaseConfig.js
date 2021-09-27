edc82ca1514002532dd74b712ef65b34c083fe83
David Renderos <djrenderos@gmail.com>
2021-09-27 17:21:28 +1000

    Login full functionalaity

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyC8yZkyV8WSV5liittTaicvfk70PI0bJpc",
+    authDomain: "test-70a1d.firebaseapp.com",
+    databaseURL: "https://test-70a1d-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "test-70a1d",
+    storageBucket: "test-70a1d.appspot.com",
+    messagingSenderId: "576707302389",
+    appId: "1:576707302389:web:1b3778711a6384316b46bc",
+    measurementId: "G-PSSWC56VW5"
+}
